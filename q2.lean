7de560e82c67e4f7d74995c66d29c1842a340ed1
Ezequiel Barrios <lbarrios@users.noreply.github.com>
2016-02-25 17:24:22 -0300

    Create q2.lean

---
+++ q2.lean
@@ -0,0 +1,28 @@
+open classical
+variables (A : type)(p q : A → Prop)
+variable r: Prop
+
+--------------
+-- example 1
+--------------
+example: (∃x, p x ∨ q x) ↔ (∃x, p x) ∨ (∃x, q x) := sorry
+
+--------------
+-- example 2
+--------------
+example: (¬∀x, p x) ↔ (∃x, ¬ p x) := sorry
+
+--------------
+-- example 3
+--------------
+example: (∃x, p x) ↔ ¬ (∀x, ¬ p x) := sorry
+
+--------------
+-- example 4
+--------------
+example: (a: A) : (∃x, r → p x) ↔ (r → ∃x, p x) := sorry
+
+--------------
+-- example 5
+--------------
+example: (∃x: A, true) → (∀x, p x ∧ r) → (∀x, p x) ∧ r := sorry
