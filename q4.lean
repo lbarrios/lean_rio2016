a800443721f939aba5c254992a7724303a35ee9c
Ezequiel Barrios <lbarrios@users.noreply.github.com>
2016-02-27 17:23:56 -0300

    Update q4.lean

--- q4.lean
+++ q4.lean
@@ -17,4 +17,11 @@ premise symmetric : ∀ {a b : A},
 premise serial : ∀ a : A, ∃ b : A,
   R a b
 
-lemma refl : ∀ a, R a a := sorry
+lemma refl : ∀ a, R a a :=
+  have H₁ : (∀ a:A, ∃ b:A, R a b), from serial,
+  take a₁ : A,
+  have H₂ : ∃ b : A, R a₁ b, from (H₁ a₁),
+  obtain b₁ (H₃ : R a₁ b₁), from H₂,
+  have H₄ : R b₁ a₁, from symmetric H₃,
+  have H₅ : R a₁ a₁, from transitive H₃ H₄,
+  H₅
